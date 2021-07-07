692401c78ab8575f010b47d56a5ce4a25ec53c5d
jiayuewu16 <jwu7@caltech.edu>
2021-07-07 15:57:43 -0700

    Can upload post to the Parse database.

--- app/src/main/java/com/example/instagram/activities/MainActivity.java
+++ app/src/main/java/com/example/instagram/activities/MainActivity.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.instagram.databinding.ActivityMainBinding;
+import com.example.instagram.models.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +36,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //queryPosts();
         Intent intent = new Intent(MainActivity.this, CreateActivity.class);
         startActivity(intent);
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                }
+                else {
+                    for (Post post : posts) {
+                        Log.i(TAG, "Post: " + post.getDescription());
+                    }
+                }
+            }
+        });
     }
 }
